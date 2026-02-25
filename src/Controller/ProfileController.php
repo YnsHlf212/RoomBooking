cc3686bf4607bb089138e85d1d809194e625e3c1
Yanis <yanis.adidi06@gmail.com>
2026-02-25 12:19:44 +0100

    Jalon 11 - Création des pages Twig : accueil, dashboard, profil utilisateur

---
+++ src/Controller/ProfileController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\ProfileType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[Route('/profile')]
+#[IsGranted('ROLE_USER')]
+class ProfileController extends AbstractController
+{
+    #[Route('/', name: 'app_profile')]
+    public function index(): Response
+    {
+        return $this->render('profile/index.html.twig', [
+            'user' => $this->getUser(),
+        ]);
+    }
+
+    #[Route('/edit', name: 'app_profile_edit', methods: ['GET', 'POST'])]
+    public function edit(
+        Request $request,
+        EntityManagerInterface $entityManager,
+        UserPasswordHasherInterface $hasher
+    ): Response
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(ProfileType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Nouveau mot de passe si renseigné
+            $newPassword = $form->get('plainPassword')->getData();
+            if ($newPassword) {
+                $user->setPassword($hasher->hashPassword($user, $newPassword));
+            }
+
+            $entityManager->flush();
+            $this->addFlash('success', 'Profil mis à jour avec succès !');
+            return $this->redirectToRoute('app_profile');
+        }
+
+        return $this->render('profile/edit.html.twig', [
+            'form' => $form,
+        ]);
+    }
+}
